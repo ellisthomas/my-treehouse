d543205b4b8eb159f1c918a1eaa21b8acad12375
ellisthomas <ellisthomas@gmail.com>
2017-03-07 20:00:24 -0600

    got treehouse badges to print to DOM

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+var treeContainer = document.getElementById("treeContainer");
+
+function makeDom(xhrData) {
+	var treehouseString = ""
+	var currentBadge;
+	for (var i = 0; i < xhrData.badges.length; i++) {
+		currentBadge = xhrData.badges[i];
+
+	treehouseString += `<div class="col-sm-6 col-md-4">`;
+	treehouseString += `<div class="thumbnail">`;
+	treehouseString += `<img src="${currentBadge.icon_url}" alt="badge">`;
+	treehouseString += `<h3>${currentBadge.name}</h3>`;
+	treehouseString += `</div></div>`;
+  
+}
+		treeContainer.innerHTML = treehouseString;
+}
+
+
 function executeThisCodeAfterFileLoaded() {
 	var data = JSON.parse(this.responseText)
+	console.log("Data returned", data);
+	makeDom(data);
 }
 
 function executeThisCodeAfterFileFails(){
